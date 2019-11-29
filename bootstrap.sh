d120c4afcc9f6e06ce19f9e06bfbadde5864eadf
Ari Davies <3873555+kussic@users.noreply.github.com>
2019-11-29 20:24:44 +0900

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -108,9 +108,9 @@ cd $HOME/.config/kalima/scripts
 ls -1A | while read file; do echo -e \"\$file \n\t \$(grep \"#DESCRIPTION:\" \$file | sed 's/#DESCRIPTION: //g')\";done
 cd - > /dev/null 2>&1
 }
-if [ -f /root/.config/kalima/scripts/$1 ] 
+if [ -f /root/.config/kalima/scripts/\$1 ] 
 	then
-		bash /root/.config/kalima/scripts/$1
+		bash /root/.config/kalima/scripts/\$1
 	else
 		usage
 		exit 1
