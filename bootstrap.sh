ed35ca2a24c200988c696f85e24e682f6e024d39
Kadir Yamamoto <kadir.yamamoto@tohmatsu.co.jp>
2019-12-09 15:25:17 +0900

    Actually activate gyarados

--- bootstrap.sh
+++ bootstrap.sh
@@ -173,7 +173,7 @@ if [ $ERROR -ne 0 ]; then
 fi
 
 echoAction "Installing Oh-My-Fish"
-(git clone -q https://github.com/oh-my-fish/oh-my-fish /tmp/oh-my-fish && /tmp/oh-my-fish/bin/install --offline --noninteractive --yes && echo 'set -g VIRTUALFISH_PYTHON "/usr/bin/python"' >>  /root/.config/omf/before.init.fish && echo 'set -g VIRTUALFISH_PLUGINS "auto_activation"' >>  /root/.config/omf/before.init.fish && echo 'set -g VIRTUALFISH_HOME $HOME/.local/share/virtualenvs/' >>  /root/.config/omf/before.init.fish && echo "set -xg GOPATH $HOME/Tools/go" >>  /root/.config/omf/init.fish && /usr/bin/fish -c "omf install https://github.com/rTD-JP/gyarados extract rvm virtualfish") > /dev/null 2>&1
+(git clone -q https://github.com/oh-my-fish/oh-my-fish /tmp/oh-my-fish && /tmp/oh-my-fish/bin/install --offline --noninteractive --yes && echo 'set -g VIRTUALFISH_PYTHON "/usr/bin/python"' >>  /root/.config/omf/before.init.fish && echo 'set -g VIRTUALFISH_PLUGINS "auto_activation"' >>  /root/.config/omf/before.init.fish && echo 'set -g VIRTUALFISH_HOME $HOME/.local/share/virtualenvs/' >>  /root/.config/omf/before.init.fish && echo "set -xg GOPATH $HOME/Tools/go" >>  /root/.config/omf/init.fish && /usr/bin/fish -c "omf install https://github.com/rTD-JP/gyarados extract rvm virtualfish" && /usr/bin/fish -c "omf theme gyarados") > /dev/null 2>&1
 ERROR=$?
 if [ $ERROR -ne 0 ]; then
    echoError "Oh-My-Fish could not be installed"
